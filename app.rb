5389ad5dde0db875a52c4a892cdd8b1ffa9f21c4
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-06-06 18:05:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  get '/team' do
+    erb :team
+  end
+
+  post '/newteam' do
+    # puts params
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @pf = params[:pf]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
 
 end
